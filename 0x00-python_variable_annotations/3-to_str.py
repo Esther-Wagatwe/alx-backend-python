2d4ed3f07bc14b2e91861468893bdbc8836e6be7
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-08-15 12:26:12 +0300

    To string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that provides a type-annotated function"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function converts a float to a string and returns the result.
+
+    Parameters: n (float): The float to convert.
+
+    Returns: str: The string representation of n.
+    """
+    return str(n)
