5fb3b193f8676b8c10554b4e5a1dc234139427ff
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-08-19 13:42:37 +0300

    execute multiple coroutines at the same time with async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """Waits for a random delay between 0 and max_delay"""
-    rand = random.uniform(0, max_delay)
-    await asyncio.sleep(rand)
-    return rand
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
