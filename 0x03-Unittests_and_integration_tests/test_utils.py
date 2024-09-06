1cbf57e928073fc032dd2461283dd40dfabb5937
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-06 16:36:00 +0300

    Debugging

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -47,13 +47,14 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """Test cases for the memoize decorator."""
-    def test_memoized(self):
+    def test_memoize(self):
         """Test that memoize decorator caches results."""
         class TestClass:
             """Class that defines attributes to test memoize"""
             def a_method(self):
                 """Method that returns an instance of memoize class"""
                 return 42
+
             @memoize
             def a_property(self):
                 """Method that defines a property instance of memoize"""
@@ -62,11 +63,14 @@ class TestMemoize(unittest.TestCase):
 
 
         test_instance = TestClass()
-        with patch.object(test_instance, 'a_method') as mock_a_method:
-            test_instance.a_property
-            test_instance.a_property
+        with patch.object(test_instance, 'a_method',
+                          return_value=42) as mock_a_method:
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
 
             mock_a_method.assert_called_once()
+
+            self.assertEqual(result1, result2)
 
     if __name__ == "__main__":
         unittest.main()
