8268b2760ef497d2f8ea526325b835a57bb85e61
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-06 16:19:55 +0300

    Test case for GithubOrgClient

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module to test the client module"""
+import unittest
+from typing import Dict
+from client import GithubOrgClient
+from unittest.mock import patch, MagicMock
+from utils import get_json
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test cases for the GithubOrgClient class."""
+    @parameterized.expand([
+        ("google", {"org": "google", "data": "example_data_google"}),
+        ("abc", {"org": "abc", "data": "example_data_abc"}),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name: str, expected_data: Dict,
+                 mock_json: MagicMock) -> None:
+        """Test that GithubOrgClient.org returns the expected value."""
+        mock_json.return_value = MagicMock(return_value=expected_data)
+
+        client = GithubOrgClient(org_name)
+
+        result = client.org()
+
+        mock_json.assert_called_once_with(
+            f'https://api.github.com/orgs/{org_name}')
+
+        self.assertEqual(result, expected_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
