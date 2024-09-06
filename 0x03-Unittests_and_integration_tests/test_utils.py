a6bd245f373e5c213426df49f4d3cca635bff18d
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-06 14:14:00 +0300

    using assertRaises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,5 +16,17 @@ class TestAccessNestedMap(unittest.TestCase):
         """test access_nested_map with different inputs"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that KeyError is raised for missing keys."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception),
+                         f"'{path[len(nested_map):][0]}'")
+
     if __name__ == "__main__":
         unittest.main()
