5438a11c5222635709f972336e89e3d949ac0bfd
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-06 20:40:46 +0300

    Debugging

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -72,7 +72,9 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False)])
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({}, "my_license", False),
+        ])
     def test_has_license(self, repo, license_key, exp_result):
         """Method to test GithubOrgClient.has_license function"""
 
